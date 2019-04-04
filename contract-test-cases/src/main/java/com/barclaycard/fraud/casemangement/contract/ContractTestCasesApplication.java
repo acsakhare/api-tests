2b85b7ced34657811451d80d0cbcfae34e1e6d48
Anup Sakhare <acsakhare@gmail.com>
2019-04-04 09:51:40 +0530

    Initial commit

---
+++ contract-test-cases/src/main/java/com/barclaycard/fraud/casemangement/contract/ContractTestCasesApplication.java
@@ -0,0 +1,13 @@
+package com.barclaycard.fraud.casemangement.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractTestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractTestCasesApplication.class, args);
+	}
+
+}
